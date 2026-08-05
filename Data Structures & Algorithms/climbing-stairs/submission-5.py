df4c834279edc6c1e7df0f2463f3f005cfef248d
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2026-08-06 01:06:46 +0530

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def climb_helper(i):
+            if i<=1:
+                return 1
+            if i in memo:
+                return memo[i]
+            memo[i] = climb_helper(i-1)+climb_helper(i-2)
+            return memo[i]
+        return climb_helper(n)
